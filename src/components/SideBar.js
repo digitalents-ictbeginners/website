96c8eb1bf82087b08a8c084810bae148f793b38d
ssuvanto <samu.suvanto@gmail.com>
2019-02-10 14:43:40 +0200

    Improved sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./SideBar.css";
+import menuimg from "../imgs/menu.png";
+import ximg from "../imgs/x.png";
+import logoimg from "../imgs/logo_placeholder.png";
 
 class SideBar extends React.Component {
     constructor(){
@@ -18,19 +21,18 @@ class SideBar extends React.Component {
     render(){
         const parentState = this.props.parent.state;
         const openstateClass = "sidebarWrapper" + (parentState.sidebarOpen ? " sidebarOpen" : " sidebarClosed");
-        const buttonClass = "togglebutton" + (parentState.sidebarOpen ? " buttonOpen" : " buttonClose");
+        const buttonClass = "togglebutton" + (parentState.sidebarOpen ? " buttonOpen" : " buttonClosed");
+        const buttonImg = parentState.sidebarOpen ? ximg : menuimg;
         return (
             <div className={openstateClass}>
                 <button onClick={this.handleButton} className={buttonClass}>
-                {parentState.sidebarOpen ? "⏴" : "⏵"}
+                    <img src={buttonImg} width="32px" alt=""/>
                 </button>
+                <img src={logoimg} width="240px" alt=""/>
                 <div className="sidebar">
-                    <h2>Sidebar Item 1</h2>
-                    <h2>Sidebar Item 2</h2>
-                    <h2>Sidebar Item 3</h2>
-                    <h2>Sidebar Item 4</h2>
-                    <h2>Sidebar Item 5</h2>
-                    <h2>Sidebar Item 6</h2>
+                    <h2>Nuorille</h2>
+                    <h2>Yrityksille</h2>
+                    <h2>Yhteistyötahoille</h2>
                 </div>
             </div>
         );
