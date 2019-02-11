0a32b7ece0199e36b5d0bf1db07f6ccada5625c1
ssuvanto <samu.suvanto@gmail.com>
2019-02-11 14:31:41 +0200

    navbar progress

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -9,18 +9,26 @@ class TopNavBar extends React.Component {
         this.state = {
             openIndex: 0
         }
+        this.subNavBars = [
+            "",
+            <div className="subNavBar">Bar1</div>,
+            <div className="subNavBar">Bar2</div>,
+            <div className="subNavBar">Bar3</div>,
+            <div className="subNavBar">Bar4</div>,
+            <div className="subNavBar">Bar5</div>
+        ];
     }
     render(){
         return (
             <div className="topNavBar">
                 <div className="navElementContainer">
-                    <TopNavBarElement defaultOpen/>
-                    <TopNavBarElement />
-                    <TopNavBarElement />
-                    <TopNavBarElement />
-                    <TopNavBarElement />
+                    <TopNavBarElement parent={this} id={1} defaultOpen/>
+                    <TopNavBarElement parent={this} id={2}/>
+                    <TopNavBarElement parent={this} id={3}/>
+                    <TopNavBarElement parent={this} id={4}/>
+                    <TopNavBarElement parent={this} id={5}/>
                 </div>
-                
+                {this.subNavBars[this.state.openIndex]}
             </div>
         );
     }
--- src/components/TopNavBarElement.js
+++ src/components/TopNavBarElement.js
@@ -7,11 +7,35 @@ class TopNavBarElement extends React.Component {
         this.state = {
             menuOpen: props.defaultOpen || false
         }
+        this.handleHover = this.handleHover.bind(this);
+        this.handleUnHover = this.handleUnHover.bind(this);
     }
+
+    handleHover(){
+        this.setState(s => {
+            return {
+                menuOpen: true
+            }
+        });
+        this.props.parent.setState(s => {
+            return {
+                openIndex: this.props.id
+            }
+        });
+    }
+
+    handleUnHover(){
+        this.setState(s => {
+            return {
+                menuOpen: false
+            }
+        });
+    }
+
     render(){
         const openClass = this.state.menuOpen ? "tnbElem tnbElemOpen" : "tnbElem tnbElemClosed";
         return (
-            <div className={openClass}>
+            <div onMouseEnter={this.handleHover} onMouseLeave={this.handleUnHover} className={openClass}>
                 Element
             </div>
         );
