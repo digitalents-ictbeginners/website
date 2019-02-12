b6816b298d6982ceded36ba464d43d321153332e
ssuvanto <samu.suvanto@gmail.com>
2019-02-12 12:19:21 +0200

    improved navbar functionality

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -3,31 +3,30 @@ import "./TopNavBar.css";
 import "./TopNavBarElement";
 import TopNavBarElement from "./TopNavBarElement";
 import SubNavBar from "./SubNavBar";
+import logo from "../imgs/logo_placeholder.png";
 
 class TopNavBar extends React.Component {
     constructor(){
         super();
+        this.subNavBars = [
+            <SubNavBar index={0}/>,
+            <SubNavBar index={1}/>,
+            <SubNavBar index={2}/>,
+            <SubNavBar index={3}/>
+        ];
         this.state = {
             openIndex: 0
         }
-        this.subNavBars = [
-            "",
-            <SubNavBar index={1}/>,
-            <SubNavBar index={2}/>,
-            <SubNavBar index={3}/>,
-            <SubNavBar index={4}/>,
-            <SubNavBar index={5}/>
-        ];
     }
     render(){
         return (
             <div className="topNavBar">
+            <img src={logo} alt="" width="64px" />
                 <div className="navElementContainer">
-                    <TopNavBarElement parent={this} index={1} defaultOpen/>
-                    <TopNavBarElement parent={this} index={2}/>
-                    <TopNavBarElement parent={this} index={3}/>
-                    <TopNavBarElement parent={this} index={4}/>
-                    <TopNavBarElement parent={this} index={5}/>
+                    <TopNavBarElement parent={this} index={0} openid={this.state.openIndex}/>
+                    <TopNavBarElement parent={this} index={1} openid={this.state.openIndex}/>
+                    <TopNavBarElement parent={this} index={2} openid={this.state.openIndex}/>
+                    <TopNavBarElement parent={this} index={3} openid={this.state.openIndex}/>
                 </div>
                 {this.subNavBars[this.state.openIndex]}
             </div>
--- src/components/TopNavBarElement.js
+++ src/components/TopNavBarElement.js
@@ -4,19 +4,10 @@ import "./TopNavBarElement.css";
 class TopNavBarElement extends React.Component {
     constructor(props){
         super(props);
-        this.state = {
-            menuOpen: props.defaultOpen || false
-        }
         this.handleHover = this.handleHover.bind(this);
-        this.handleUnHover = this.handleUnHover.bind(this);
     }
 
     handleHover(){
-        this.setState(s => {
-            return {
-                menuOpen: true
-            }
-        });
         this.props.parent.setState(s => {
             return {
                 openIndex: this.props.index
@@ -24,18 +15,10 @@ class TopNavBarElement extends React.Component {
         });
     }
 
-    handleUnHover(){
-        this.setState(s => {
-            return {
-                menuOpen: false
-            }
-        });
-    }
-
     render(){
-        const openClass = this.state.menuOpen ? "tnbElem tnbElemOpen" : "tnbElem tnbElemClosed";
+        const openClass = this.props.index === this.props.openid ? "tnbElem tnbElemOpen" : "tnbElem tnbElemClosed";
         return (
-            <div onMouseEnter={this.handleHover} onMouseLeave={this.handleUnHover} className={openClass}>
+            <div onMouseEnter={this.handleHover} className={openClass}>
                 Element
             </div>
         );
