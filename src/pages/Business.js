005e2f08bad13815dd4c4caa5abf14d9c27c5394
Vesa Meriläinen <33903360+VMerilainen@users.noreply.github.com>
2019-02-13 11:46:19 +0200

    move content to Home view component

--- src/pages/Business.js
+++ src/pages/Business.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Content from "../components/Content";
 
 class Business extends React.Component {
     render(){
         return (
             <div>
-                <Content title="Under Construction"/>
             </div>
         );
     }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import Content from '../components/Content';
+import './Home.css';
+import data from '../Home.json';
+import image from "../imgs/office.jpg"
+import logo from '../imgs/logo_placeholder.png'
+
+const imgStyle = {
+    backgroundImage: `url(${image})`,
+    backgroundSize: 'cover'
+}
 
 export default class Home extends React.Component {
   render() {
     return (
-      <div>
-        <Content title="ICT Beginners..."/>
-      </div>
+      <main>
+        <div style={imgStyle} className="container">
+            <img src={logo} alt="" className="logo" />
+            <h4 className="subtitle">{data.slogan}</h4>
+        </div>
+      </main>
     )
   }
 }
