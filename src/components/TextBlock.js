cbc3649d3cf7a4e522639cfef8090ded672b6dfe
ssuvanto <samu.suvanto@gmail.com>
2019-02-14 09:29:41 +0200

    added paragraph separation to text input json

--- src/components/TextBlock.js
+++ src/components/TextBlock.js
@@ -5,11 +5,17 @@ class TextBlock extends React.Component {
     render(){
         const data = this.props.data;
         const subElements = data.texts.map(e => {
-            let html  = {__html: e.text};
+            const paragraphs = e.text.map(p => {
+                const html  = {__html: p};
+                return (
+                    <p dangerouslySetInnerHTML={html}></p>
+                );
+            });
+            
             return (
                 <div>
                     <h3>{e.subtitle}</h3>
-                    <p dangerouslySetInnerHTML={html}></p>
+                    {paragraphs}
                 </div>
             )
         });
