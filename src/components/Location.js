db89a25dcd4209f85719bf68894efc49195aa541
Vesa Meriläinen <33903360+VMerilainen@users.noreply.github.com>
2019-02-14 14:18:59 +0200

    fix map size to fit mobile view

--- src/components/Location.js
+++ src/components/Location.js
@@ -3,12 +3,8 @@ import data from '../Contact.json'
 import './Location.css'
 
 export default class Location extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-  
   render() {
-    const map = `<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=24.919258654117588%2C60.16585541850837%2C24.92373794317246%2C60.16704165634451&amp;layer=mapnik&amp;marker=60.16644854277959%2C24.92149829864502" style="border: 1px solid black"></iframe>`
+    const map = `<iframe width="${document.documentElement.clientWidth}" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=24.919258654117588%2C60.16585541850837%2C24.92373794317246%2C60.16704165634451&amp;layer=mapnik&amp;marker=60.16644854277959%2C24.92149829864502" style="border: 1px solid black"></iframe>`
     const html = {__html: map};
 
     return (
