7aae04641b2ab7dd7a0637262119ffc561ffa3bf
ssuvanto <samu.suvanto@gmail.com>
2019-02-26 14:48:07 +0200

    made showreel loop around

--- src/components/ShowReel/ShowReelElement.js
+++ src/components/ShowReel/ShowReelElement.js
@@ -5,6 +5,7 @@ class ShowReelElement extends React.Component {
     render(){
         const pos = this.props.reelPos;
         const id = this.props.index;
+        const len = this.props.reelLen;
         let cname;
         let clicked;
         if(pos === id){ //this is the element in view
@@ -22,13 +23,31 @@ class ShowReelElement extends React.Component {
             clicked = this.props.rtlFunc;
         }
         else if(pos === id-2){ //the element two back is in view, this is off right
-            cname = "srElem srElemOffRight"
+            cname = "srElem srElemOffRight";
         }
+        else if(pos === 0 && id === len-1){ //the first(0th) element is in view, this is last and left
+            cname = "srElem srElemLeft";
+            clicked = this.props.ltrFunc;
+        }
+        else if(pos === len-1 && id === 0){ //the last element is in view, this is first and right
+            cname = "srElem srElemRight";
+            clicked = this.props.rtlFunc;
+        }
+        else if(pos === len-2 && id === 0){
+            cname = "srElem srElemOffRight";
+        }
+        else if(pos === 0 && id === len-2){
+            cname = "srElem srElemOffLeft";
+        }
+        else if(pos === len-1 && id === 1){
+            cname = "srElem srElemOffRight";
+        }
+        //TODO: STILL NEED ANOTHER IF OR TWO
         else { //this is somewhere else so just hidden
-            cname = "srElem srElemHidden"
+            cname = "srElem srElemHidden";
         }
         return (
-            <div className={cname} onClick={clicked}></div>
+            <div className={cname} onClick={clicked}>{this.props.index}</div>
         );
     }
 }
