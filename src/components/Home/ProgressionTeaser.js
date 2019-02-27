ead5b64c88d2e9566b4a8b17810c3256f24507e7
ssuvanto <samu.suvanto@gmail.com>
2019-02-27 09:56:01 +0200

    improve teaser animation

--- src/components/Home/ProgressionTeaser.js
+++ src/components/Home/ProgressionTeaser.js
@@ -6,11 +6,13 @@ class ProgressionTeaser extends React.Component {
     constructor(){
         super();
         this.state = {
-            lineExtended: false
+            animate: false,
         }
         this.DOMNode = null;
         this.wrapper = null;
+        this.timer = null;
         this.handleScroll = this.handleScroll.bind(this);
+        this.updateState = this.updateState.bind(this);
     }
 
     componentDidMount(){
@@ -21,26 +23,35 @@ class ProgressionTeaser extends React.Component {
 
     handleScroll(){
         if(this.wrapper.scrollTop + document.documentElement.clientHeight >
-            this.DOMNode.offsetTop + 500){
-            this.setState(s => {
-                return {
-                    lineExtended: true
-                }
+            this.DOMNode.offsetTop + 300){
+                this.setState(s => {
+                    return {
+                        animate: true,
+                    }
             });
+            this.wrapper.removeEventListener("scroll", this.handleScroll);
         }
+    }
+
+    updateState(){
+        
     }
 
     render(){
         const data = this.props.data;
-        const lineClass = this.state.lineExtended ? "vLine vLineFull" : "vLine";
+        const lineClass = this.state.animate ? "vLine vLineFull" : "vLine";
+        const item1class = this.state.animate ? "ptItem1 ptFadeIn" : "ptItem1";
+        const item2class = this.state.animate ? "ptItem2 ptFadeIn" : "ptItem2";
+        const item3class = this.state.animate ? "ptItem3 ptFadeIn" : "ptItem3";
+        const item4class = this.state.animate ? "ptItem4 ptFadeIn" : "ptItem4";
         return (
             <div id={data.id} className="teaser">
                 <div className={lineClass}></div>
                 <h1>{data.title}</h1>
-                <h3>{data.item1}</h3>
-                <h3>{data.item2}</h3>
-                <h2>{data.item3}</h2>
-                <i className="fas fa-chevron-down"></i>
+                <h3 className={item1class}>{data.item1}</h3>
+                <h3 className={item2class}>{data.item2}</h3>
+                <h2 className={item3class}>{data.item3}</h2>
+                <div className={item4class}><i className="fas fa-chevron-down"></i></div>   
             </div>
         );
     }
