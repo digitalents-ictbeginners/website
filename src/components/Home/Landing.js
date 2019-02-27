30fd05d94c62ca0cfce11398ba05d39aa71481b5
ssuvanto <samu.suvanto@gmail.com>
2019-02-27 11:02:03 +0200

    add wip background image for landing area

--- src/components/Home/Landing.js
+++ src/components/Home/Landing.js
@@ -6,9 +6,11 @@ class Landing extends React.Component {
         const data = this.props.data;
         return (
             <div className="landing">
-                <h3>{data.title}</h3>
-                <h1>{data.slogan}</h1>
-                <h4>{data.subslogan}</h4>
+                <div className="landingContent">
+                    <h3>{data.title}</h3>
+                    <h1>{data.slogan}</h1>
+                    <h4>{data.subslogan}</h4>
+                </div>
             </div>
         );
     }
