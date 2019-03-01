696aa566bb718e0cb3d52ea4f9e636d43112e0c8
ssuvanto <samu.suvanto@gmail.com>
2019-03-01 10:59:41 +0200

    overhaul footer, include important people

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,30 @@
-import React from 'react'
-import './Footer.css'
-import logo from '../imgs/logo_placeholder.png'
+import React from "react"
+import {Link} from "react-router-dom";
+import "./Footer.css"
 
 class Footer extends React.Component {
   render() {
     return (
       <footer>
-        <div className="contactInfo">
-          <p>
-            Lapinlahdenkatu 16
-            <br />
-            00180 HELSINKI
-            <br />
-            sähköposti@email.fi
-          </p>
+        <div className="ftContent">
+          <div className="ftPerson">
+            <h4>Työvalmentaja</h4>
+            <div className="ftFaceImg"></div>
+            <h4>Arto Aitta</h4>
+          </div>
+          <div className="ftPerson">
+            <h4>Kouluttaja</h4>
+            <div className="ftFaceImg"></div>
+            <h4>Krishna Regmi</h4>
+          </div>
+          <div className="ftLocation">
+            <i class="fas fa-map-marker-alt fa-3x"></i><br/>
+            <span>Lapinlahdenkatu 16</span><br/>
+            <span>00180 HELSINKI</span><br/>
+            <span>Puh. 09 123 4567</span><br/>
+            <Link to="/yhteystiedot">Lisää yhteystietoja</Link>
+          </div>
         </div>
-        <img className="footLogo" src={logo} alt="logo" width="200px" />
       </footer>
     )
   }
