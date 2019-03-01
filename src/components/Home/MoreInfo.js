f06c8ca3e1e9f3f5cace6f883016f5a620ec840e
ssuvanto <samu.suvanto@gmail.com>
2019-03-01 10:00:57 +0200

    implement responsive blog preview structure

--- src/components/Home/MoreInfo.js
+++ src/components/Home/MoreInfo.js
@@ -4,6 +4,7 @@ import "./MoreInfo.css";
 class MoreInfo extends React.Component {
     render(){
         const data = this.props.data;
+        //Blog preview cards should eventually be independent components
         return (
             <div id={data.id} className="moreInfoBlock">
                 <div className="moreInfoInner">
@@ -11,7 +12,16 @@ class MoreInfo extends React.Component {
                     <p>{data.text}</p>
                     <h3>{data.toblog}</h3>
                 </div>
-                <div className="blogtemp">Blog preview tähän</div>
+                <div className="blogPreview">
+                    <div className="blogPreviewCardContainer">
+                        <div className="blogPreviewCard"></div>
+                        <div className="blogPreviewCard"></div>
+                        <div className="blogPreviewCard"></div>
+                        <div className="blogPreviewCard"></div>
+                        <div className="blogPreviewCard"></div>
+                    </div>
+                    <div className="blogLinkCard"></div>
+                </div>
             </div>
         );
     }
