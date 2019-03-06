44b929d3149d59bf6688974b11b320d70dc9c3eb
ssuvanto <samu.suvanto@gmail.com>
2019-03-06 13:51:35 +0200

    improve contacts page responsivity although still wip because hover feature not great for mobile

--- src/components/People.js
+++ src/components/People.js
@@ -2,17 +2,17 @@ import React from "react";
 import "./People.css";
 
 class People extends React.Component{
-    //Also maybe add a picture for each person
+    
     render(){
         const people = this.props.data.people.peoplecards.map(e => {
             return (
                 <div className="person">
                     <div className="personText">
-                        <i class="fas fa-user fa-2x"></i>
+                        <i className="fas fa-user fa-2x"></i>
                         <h3>{e.name}</h3>
                         <h4>{e.title}</h4>
-                        <h4><i class="fas fa-phone"></i> {e.landline}</h4>
-                        <h4><i class="fas fa-mobile-alt"></i> {e.mobile}</h4>
+                        <h4><i className="fas fa-phone"></i> {e.landline}</h4>
+                        <h4><i className="fas fa-mobile-alt"></i> {e.mobile}</h4>
                         <h4>{e.email}</h4>
                     </div>
                 </div>
