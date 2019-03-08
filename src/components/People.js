d304a57ed4f63389ee521177162794c7d231c103
ssuvanto <samu.suvanto@gmail.com>
2019-03-08 13:23:59 +0200

    revamp contacts page people section

--- src/components/People.js
+++ src/components/People.js
@@ -1,21 +1,13 @@
 import React from "react";
 import "./People.css";
+import Person from "./Person";
 
 class People extends React.Component{
     
     render(){
         const people = this.props.data.people.peoplecards.map(e => {
             return (
-                <div className="person">
-                    <div className="personText">
-                        <i className="fas fa-user fa-2x"></i>
-                        <h3>{e.name}</h3>
-                        <h4>{e.title}</h4>
-                        <h4><i className="fas fa-phone"></i> {e.landline}</h4>
-                        <h4><i className="fas fa-mobile-alt"></i> {e.mobile}</h4>
-                        <h4>{e.email}</h4>
-                    </div>
-                </div>
+                <Person key={e.id} data={e} />
             );
         });
 
---
+++ src/components/Person.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Person.css";
+
+class Person extends React.Component {
+
+    render(){
+        const data = this.props.data;
+        return (
+            <div className="person">
+                <div className="personText">
+                    <h3>{data.name}</h3>
+                    <h4>{data.title}</h4>
+                    <h4><i className="fas fa-phone"></i> {data.landline}</h4>
+                    <h4><i className="fas fa-mobile-alt"></i> {data.mobile}</h4>
+                    <h4>{data.email}</h4>
+                </div>
+                <div className="personImg">Picture here</div>
+                <div className="personQuote">
+                    <i className="fas fa-quote-left fa-2x"></i>
+                    <p>Some nice quote text here</p>
+                    <i className="fas fa-quote-right fa-2x personRightQuote"></i>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Person;
