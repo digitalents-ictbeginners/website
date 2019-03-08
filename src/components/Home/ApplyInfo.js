b2004ff8ea5e49c39c46168e579fb94c6f44a492
ssuvanto <samu.suvanto@gmail.com>
2019-03-08 10:35:55 +0200

    put in new content texts and adjust structure appropriately

--- src/components/Home/ApplyInfo.js
+++ src/components/Home/ApplyInfo.js
@@ -4,15 +4,23 @@ import "./ApplyInfo.css";
 class ApplyInfo extends React.Component {
     render(){
         const data = this.props.data;
+        const texts = data.text.map(e => {
+            return (
+                <p key={e.id} class="aiParagraph">
+                    <span className="aiBoldText">{e.bold}</span>
+                    <span className="aiNormalText"> {e.normal}</span>
+                </p>
+            );
+        });
         return (
             <div id={data.id} className="applyInfo">
                 <div className="aiUnderlined"><h2>{data.interested}</h2></div>
                 <h1>{data.applynow}</h1>
                 <div className="aiContentWrapper">
                     <div className="aiContent">
-                        <div className="aiText">
+                        <div className="aiTextBox">
                             <h2>{data.process}</h2>
-                            <p>{data.text}</p>
+                            {texts}
                         </div>
                         <div className="aiToApply">
                             <h2>{data.toapply}</h2>
