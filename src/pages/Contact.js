9dc4bfc1e54698a49ee1d37b4c0495d871ce861a
ssuvanto <samu.suvanto@gmail.com>
2019-03-08 09:52:56 +0200

    enable language switching on contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Contact.css";
-import data from "../data/Contact.json";
+import dataFI from "../data/Contact_fi.json";
+import dataEN from "../data/Contact_en.json";
 import Location from "../components/Location";
 import People from "../components/People";
 
 class Contact extends React.Component {
 
     render(){
+        const data = this.props.english ? dataEN : dataFI;
         return (
             <div>
                 <Location data={data} />
