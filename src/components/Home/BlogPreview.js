4a966bfbe473e7776b650ee7e3fca47a4b05b2cc
ssuvanto <samu.suvanto@gmail.com>
2019-03-13 10:53:49 +0200

    add some example/placeholder content to blogpreview cards

--- src/components/Home/BlogPreview.js
+++ src/components/Home/BlogPreview.js
@@ -6,13 +6,33 @@ class BlogPreview extends React.Component {
         return (
             <div className="blogPreview">
                 <div className="blogPreviewCardContainer">
-                    <div className="blogPreviewCard"></div>
-                    <div className="blogPreviewCard"></div>
-                    <div className="blogPreviewCard"></div>
-                    <div className="blogPreviewCard"></div>
-                    <div className="blogPreviewCard"></div>
+                    <div className="blogPreviewCard">
+                        <h4>Maanantai 11.3</h4>
+                        <h3>Aamupala</h3>
+                        <p>Maanantaisin Digitalents Academy kokoontuu aamupalan 
+                            ääreen keskustelemaan...</p>
+                    </div>
+                    <div className="blogPreviewCard">
+                        <h4>Keskiviikko 13.3</h4>
+                        <h3>Otsikko</h3>
+                        <p>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
+                            ipsum lorem ipsum...</p>
+                    </div>
+                    <div className="blogPreviewCard">
+                        <h4>Perjantai 15.3</h4>
+                        <h3>Otsikko</h3>
+                        <p>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
+                            ipsum lorem ipsum...</p>
+                    </div>
+                    <div className="blogPreviewCard">
+                        <h2>Linkit<br/>&amp;<br/>Vinkit</h2>
+                        <h4>Hyödylliset avut ohjelmointiin</h4>
+                    </div>
                 </div>
-                <div className="blogLinkCard"></div>
+                <div className="blogLinkCard">
+                    <h2>Blogiin</h2>
+                    <i className="fas fa-long-arrow-alt-right fa-5x"></i>
+                </div>
             </div>
         );
     }
