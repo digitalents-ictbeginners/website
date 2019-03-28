d1034bbfe4325c17dbe4e14741fa7fd3ba293c65
ssuvanto <samu.suvanto@gmail.com>
2019-03-28 10:23:51 +0200

    Updates

---
+++ precache-manifest.ff108a0eaf431a56993e20a0b5dd896e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f9fe68ed0eab67ada280f7b9d652da1",
+    "url": "/website/static/media/placeholder2.0f9fe68e.png"
+  },
+  {
+    "revision": "9463e21545b49d0a0249",
+    "url": "/website/static/css/main.e361c7f5.chunk.css"
+  },
+  {
+    "revision": "7cf14776b7ccb3be0c0ae49c7c3d587e",
+    "url": "/website/static/media/office.7cf14776.jpg"
+  },
+  {
+    "revision": "4c3acb893124376baf6b",
+    "url": "/website/static/js/1.4c3acb89.chunk.js"
+  },
+  {
+    "revision": "4568e64d5715df9ed1c7ed35d4cacec4",
+    "url": "/website/static/media/landingbg.4568e64d.jpg"
+  },
+  {
+    "revision": "62d1b246b0126757d972",
+    "url": "/website/static/js/2.62d1b246.chunk.js"
+  },
+  {
+    "revision": "96adef8aec8f8506e79b",
+    "url": "/website/static/js/3.96adef8a.chunk.js"
+  },
+  {
+    "revision": "47709952e3c8015e772a",
+    "url": "/website/static/js/runtime~main.47709952.js"
+  },
+  {
+    "revision": "efd5160a6c41e7b64714827f7305386b",
+    "url": "/website/static/media/blogplaceholder.efd5160a.png"
+  },
+  {
+    "revision": "4aaeacf370b0a72d8cccbab6c8c828b9",
+    "url": "/website/static/media/placeholder0.4aaeacf3.png"
+  },
+  {
+    "revision": "f94c51173a51ffc9575750d85e088a34",
+    "url": "/website/static/media/placeholder1.f94c5117.png"
+  },
+  {
+    "revision": "9463e21545b49d0a0249",
+    "url": "/website/static/js/main.9463e215.chunk.js"
+  },
+  {
+    "revision": "3cde41adb9e978d5a1daca39c004f729",
+    "url": "/website/static/media/placeholder3.3cde41ad.png"
+  },
+  {
+    "revision": "8605c96a4b859ae9f87b38bae3d1be00",
+    "url": "/website/static/media/placeholder4.8605c96a.png"
+  },
+  {
+    "revision": "8874d838b8709416a4cc65d52e41e606",
+    "url": "/website/static/media/placeholder5.8874d838.png"
+  },
+  {
+    "revision": "df350c5f114c77b8cda92a3638e37446",
+    "url": "/website/static/media/elli.df350c5f.png"
+  },
+  {
+    "revision": "c958f2deb3166c034d31e52f5f13734a",
+    "url": "/website/static/media/krishna.c958f2de.png"
+  },
+  {
+    "revision": "ae681bf0ac345e7658ca31fc64437b0d",
+    "url": "/website/static/media/DTA_isologo_musta.ae681bf0.svg"
+  },
+  {
+    "revision": "6a4e71203e916f63f26084d733f230d8",
+    "url": "/website/static/media/DTA_pienilogo_musta.6a4e7120.svg"
+  },
+  {
+    "revision": "64114bb7aa254368bb337d561c2ad088",
+    "url": "/website/static/media/DTA_pienilogo_mustapohja.64114bb7.svg"
+  },
+  {
+    "revision": "62d1b246b0126757d972",
+    "url": "/website/static/css/2.9c4429d8.chunk.css"
+  },
+  {
+    "revision": "4c3acb893124376baf6b",
+    "url": "/website/static/css/1.02277abe.chunk.css"
+  },
+  {
+    "revision": "a1e7f012d2f8ae5095ac48510c605e12",
+    "url": "/website/index.html"
+  }
+];
